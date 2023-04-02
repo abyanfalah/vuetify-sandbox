3562288abb6c6f4dd3f6694eaced101cde3e7441
Abyan Falah <abyanf410@gmail.com>
2023-04-03 03:07:01 +0700

    Update GalleryApp.vue: randomizer

--- src/apps/GalleryApp.vue
+++ src/apps/GalleryApp.vue
@@ -2,15 +2,19 @@
 import { computed } from '@vue/reactivity';
 import { onMounted, ref, watch } from 'vue';
 import ProgressCircular from '@/components/ProgressCircular.vue';
-
+import ModalConfirmRandomize from '@/components/galleryapp/ModalConfirmRandomize.vue';
 
 const numberOfImagesToDisplay = ref(5);
 const columnNumber = ref(3);
 const randomizerFactor = ref(1);
 const colorize = ref(true);
+const randomizeDialog = ref(false);
 
 function changeRandomizer() {
+  randomizeDialog.value = false;
   randomizerFactor.value = Math.floor(Math.random() * 10);
+
+  // show a nice  overlay with progress
 }
 
 const states = computed(() => {
@@ -64,8 +68,29 @@ onMounted(() => {
         </v-btn-toggle>
 
         <!-- randomize btn -->
-        <v-btn icon="mdi-dice-3-outline"
-               color=""></v-btn>
+        <v-btn class="ms-1"
+               icon="mdi-dice-3-outline">
+          <v-icon icon="mdi-dice-3-outline"></v-icon>
+          <v-dialog activator="parent"
+                    v-model="randomizeDialog"
+                    persistent
+                    width="auto">
+            <v-card class="pa-4">
+              <v-card-text>
+                <p>
+                  Randomize the images?
+                </p>
+                <span class="text-grey text-caption">You probably won't see these images anymore.</span>
+              </v-card-text>
+              <v-card-actions class="d-flex justify-space-around">
+                <v-btn variant="outlined"
+                       color="red"
+                       @click="changeRandomizer">Yes</v-btn>
+                <v-btn @click="randomizeDialog = false">No</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </v-btn>
 
         <!-- zoom out btn -->
         <v-btn icon="mdi-magnify-minus-outline"
@@ -83,23 +108,24 @@ onMounted(() => {
 
     </v-app-bar>
 
-
-    <v-card flat
-            class="pa-3">
-      <v-row class="d-flex">
-        <v-col v-for="n in numberOfImagesToDisplay"
-               :cols="columnNumber">
-          <v-card elevation="3">
-            <v-img :src="`https://picsum.photos/id/${randomizerFactor * 19 + n}/200${!colorize ? '?grayscale' : ''}`"
-                   aspect-ratio="1"
-                   :cover="true">
-              <template v-slot:placeholder>
-                <ProgressCircular />
-              </template>
-            </v-img>
-          </v-card>
-        </v-col>
-      </v-row>
-    </v-card>
+    <v-lazy transition="scroll-x-transition">
+      <v-card flat
+              class="pa-3">
+        <v-row class="d-flex">
+          <v-col v-for="n in numberOfImagesToDisplay"
+                 :cols="columnNumber">
+            <v-card elevation="3">
+              <v-img :src="`https://picsum.photos/id/${randomizerFactor * 19 + n}/200${!colorize ? '?grayscale' : ''}`"
+                     aspect-ratio="1"
+                     :cover="true">
+                <template v-slot:placeholder>
+                  <ProgressCircular />
+                </template>
+              </v-img>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-lazy>
   </div>
 </template>
