93fe09229b84ffefa4fb5dca6f20728b67fcaedc
Abyan Falah <abyanf410@gmail.com>
2023-04-02 21:49:12 +0700

    Update App.vue: store last app to localstorage

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,53 @@
 <script setup>
-import { ref } from 'vue';
+import { onBeforeMount, ref, watch } from 'vue';
 
 import SideBar from './components/SideBar.vue';
-import GalleryApp from  './components/GalleryApp.vue'
-import TestPage from  './components/TestPage.vue'
-import TodoApp from  './components/TodoApp.vue'
-import CounterApp from './components/CounterApp.vue';
 
-const selectedApp = ref(null)
+import GalleryApp from './apps/GalleryApp.vue';
+import TestPage from './components/TestPage.vue';
+import TodoApp from './apps/TodoApp.vue';
+import CounterApp from './apps/CounterApp.vue';
+import WeatherApp from './apps/WeatherApp.vue';
+
+const selectedApp = ref(null);
 
 const apps = {
   'gallery': GalleryApp,
-  'testpage' : TestPage,
+  'testpage': TestPage,
   'todo': TodoApp,
-  'counter' : CounterApp
-}
+  'counter': CounterApp,
+  'weather': WeatherApp,
+};
 
 function changePage(to) {
-  console.log('to page', to)
-  selectedApp.value = to
+  selectedApp.value = to;
 }
+
+watch(selectedApp, () => {
+  localStorage.setItem('vuetify-sandbox-current-app', selectedApp.value);
+});
+
+onBeforeMount(() => {
+  selectedApp.value = localStorage.getItem('vuetify-sandbox-current-app') ?? null;
+})
 
 </script>
 
 <template>
-<v-app>
-  <!-- sidebar -->
-  <SideBar @select-page="changePage" />
+  <v-app>
+    <!-- sidebar -->
+    <SideBar @select-page="changePage" />
 
-  <!-- main content -->
-  <v-main>
-    <component
-      v-if="selectedApp"
-      class="mx-3 my-2 px-3 py-2"
-      :is="apps[selectedApp]"
-    />
+    <!-- main content -->
+    <v-main>
+      <component v-if="selectedApp"
+                 class="mx-3 my-2 px-3 py-2"
+                 :is="apps[selectedApp]" />
 
-    <div class="mx-3 my-2 px-3 py-2" v-else>
-      <v-sheet align="center">Hello World!</v-sheet>
-    </div>
-  </v-main>
-</v-app>
+      <div class="mx-3 my-2 px-3 py-2"
+           v-else>
+        <v-sheet align="center">Hello World!</v-sheet>
+      </div>
+    </v-main>
+  </v-app>
 </template>
