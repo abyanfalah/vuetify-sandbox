dda58e5ca91084e2b32f6bbf5330ea962540eb4e
Abyan Falah <abyanf410@gmail.com>
2023-04-03 13:49:40 +0700

    Update WeatherApp.vue: background color by temperature

--- src/apps/WeatherApp.vue
+++ src/apps/WeatherApp.vue
@@ -2,12 +2,28 @@
 import { onMounted, ref, watch } from 'vue';
 import weatherService from '../services/weatherApi';
 import ProgressCircular from '@/components/ProgressCircular.vue';
+import { computed } from '@vue/reactivity';
 
 const locationQuery = ref();
 const weatherData = ref();
 const isSearching = ref(false);
 const location = ref({});
+const isCelsius = ref(true);
 
+const tempColor = computed(() => {
+  if (!weatherData.value) return 'white';
+
+  const temperature = weatherData.value.current.temp_c;
+
+  const coldWeather = temperature < 20;
+  const normalWeather = temperature < 30;
+
+  if (coldWeather) return 'blue';
+
+  if (normalWeather) return 'green';
+
+  return 'red';
+});
 
 async function getWeatherData() {
   if (!locationQuery.value) {
@@ -51,35 +67,51 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
-  <h1>Weather App</h1>
-  <v-container>
+  <div class="h-100"
+       :class="`bg-${tempColor}-lighten-2`">
+    <v-app-bar title="Weather app"
+               flat>
+      <template v-slot:append>
+        <v-switch density="comfortable"
+                  hide-details
+                  v-model="isCelsius"
+                  :color="isCelsius ? 'green' : 'yellow'"
+                  prepend-icon="mdi-temperature-celsius"
+                  append-icon="mdi-temperature-fahrenheit"></v-switch>
+
+      </template>
+    </v-app-bar>
+
+
+    <v-container>
 
       <!-- locationQuery input -->
       <v-row>
         <v-col cols="4"
                class="mx-auto">
-          <v-sheet elevation="4"
-                   class="rounded">
-            <v-text-field label="Search location"
-                          placeholder="e.g. Tokyo"
-                          v-model="locationQuery"
-                          append-inner-icon="mdi-magnify"
-                          @click:append-inner="getWeatherData"
-                          @keydown.enter="getWeatherData"
-                          clear-icon="mdi-close-circle"
-                          clearable
-                          autofocus
-                          density="compact"
-                          hint="city, zip code, coordinate, or 'auto:ip'"></v-text-field>
-          </v-sheet>
+          <v-text-field color="blue"
+                        bg-color="white"
+                        label="Search location"
+                        placeholder="e.g. Tokyo"
+                        v-model="locationQuery"
+                        append-inner-icon="mdi-magnify"
+                        @click:append-inner="getWeatherData"
+                        @keydown.enter="getWeatherData"
+                        clear-icon="mdi-close-circle"
+                        clearable
+                        autofocus
+                        density="comfortable"
+                        hint="city, zip code, coordinate, or 'auto:ip'"></v-text-field>
         </v-col>
       </v-row>
 
       <!-- weather and temperature -->
       <v-row>
-        <v-col class="mx-auto">
-          <v-container align="center">
+        <v-col>
+          <v-card align="center"
+                  elevation="24"
+                  max-width="700px"
+                  class="pt-5 pb-10 mx-auto">
             <ProgressCircular v-if="isSearching" />
             <div v-else-if="weatherData"
                  class="d-flex flex-column align-center">
@@ -95,29 +127,27 @@ onMounted(() => {
                   </span>
 
                   <div class="text-h1 my-10">
-                    {{ weatherData.current.temp_c }} C
+                    {{ weatherData.current.temp_c }} <sup>
+                      <small class="font-weight">°C</small>
+                    </sup>
                   </div>
 
                   <span class="text-h5">
                     {{ weatherData.current.condition.text }}
                   </span>
                 </v-col>
-
-                <!-- <v-col>
-                                                                                                                    {{ weatherData.current }}
-                                                                                                                  </v-col> -->
               </v-row>
 
 
 
             </div>
-          </v-container>
+          </v-card>
         </v-col>
       </v-row>
 
       <!-- other details -->
       <div v-if="weatherData">
-        <!-- {{ weatherData.current }} -->
+        {{ weatherData.current }}
       </div>
     </v-container>
 
