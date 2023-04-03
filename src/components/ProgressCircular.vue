f9b6a63f5b9414b4b61f1b21bb6d6eb10d7e2271
Abyan Falah <abyanf410@gmail.com>
2023-04-03 13:49:43 +0700

    Update ProgressCircular.vue

--- src/components/ProgressCircular.vue
+++ src/components/ProgressCircular.vue
@@ -1,8 +1,10 @@
 <template>
-  <v-row class="fill-height ma-0"
+  <v-row class="d-flex align-center mt-16"
          align="center"
          justify="center">
     <v-progress-circular indeterminate
+                         size="128"
+                         width="12"
                          color="orange lighten-1"></v-progress-circular>
   </v-row>
 </template>
