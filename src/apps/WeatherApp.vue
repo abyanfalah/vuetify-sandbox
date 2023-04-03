8fed64a0e1d8d915befbdde400510f30f5373209
Abyan Falah <abyanf410@gmail.com>
2023-04-03 10:05:41 +0700

    persist weather app states to localstorage

--- src/apps/WeatherApp.vue
+++ src/apps/WeatherApp.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import weatherService from '../services/weatherApi';
 import ProgressCircular from '@/components/ProgressCircular.vue';
 
@@ -27,32 +27,52 @@ async function getWeatherData() {
   }
 }
 
-const state = (() => {
+const states = (() => {
   return {
     locationQuery: locationQuery.value,
+    weatherData: weatherData.value,
+    location: location.value
 
   };
+
+});
+
+watch(states, (newState) => {
+  localStorage.setItem("weatherApp", JSON.stringify(newState));
+});
+
+onMounted(() => {
+  const storedState = JSON.parse(localStorage.getItem("weatherApp"));
+  location.value = storedState.location ?? location.value;
+  locationQuery.value = storedState.locationQuery ?? locationQuery.value;
+  weatherData.value = storedState.weatherData ?? weatherData.value;
 })
 
 </script>
 
 <template>
   <div>
-    <h1>Weather App</h1>
-    <v-container>
+  <h1>Weather App</h1>
+  <v-container>
 
       <!-- locationQuery input -->
       <v-row>
         <v-col cols="4"
                class="mx-auto">
-          <v-text-field label="Search location"
-                        placeholder="e.g. Tokyo"
-                        v-model="locationQuery"
-                        append-inner-icon="mdi-magnify"
-                        @click:append-inner="getWeatherData"
-                        @keydown.enter="getWeatherData"
-                        clear-icon="mdi-close-circle"
-                        clearable></v-text-field>
+          <v-sheet elevation="4"
+                   class="rounded">
+            <v-text-field label="Search location"
+                          placeholder="e.g. Tokyo"
+                          v-model="locationQuery"
+                          append-inner-icon="mdi-magnify"
+                          @click:append-inner="getWeatherData"
+                          @keydown.enter="getWeatherData"
+                          clear-icon="mdi-close-circle"
+                          clearable
+                          autofocus
+                          density="compact"
+                          hint="city, zip code, coordinate, or 'auto:ip'"></v-text-field>
+          </v-sheet>
         </v-col>
       </v-row>
 
@@ -64,21 +84,32 @@ const state = (() => {
             <div v-else-if="weatherData"
                  class="d-flex flex-column align-center">
 
-              <span class="text-h3">
-                {{ location.name }}
-              </span>
+              <v-row>
+                <v-col class="d-flex flex-column align-center">
+                  <span class="text-h3">
+                    {{ location.name }} <img :src="weatherData.current.condition.icon"
+                         alt="">
+                  </span>
+                  <span class="text-grey">
+                    {{ `${location.region}, ${location.country}` }}
+                  </span>
 
-              <span>
-                {{ `${location.region}, ${location.country}` }}
-              </span>
+                  <div class="text-h1 my-10">
+                    {{ weatherData.current.temp_c }} C
+                  </div>
 
-              <img :src="weatherData.current.condition.icon"
-                   width="300"
-                   alt="">
+                  <span class="text-h5">
+                    {{ weatherData.current.condition.text }}
+                  </span>
+                </v-col>
 
-              <span>
-                {{ weatherData.current.condition.text }}
-              </span>
+                <!-- <v-col>
+                                                                                                                    {{ weatherData.current }}
+                                                                                                                  </v-col> -->
+              </v-row>
+
+
+
             </div>
           </v-container>
         </v-col>
@@ -86,7 +117,7 @@ const state = (() => {
 
       <!-- other details -->
       <div v-if="weatherData">
-        {{ weatherData.current }}
+        <!-- {{ weatherData.current }} -->
       </div>
     </v-container>
 
