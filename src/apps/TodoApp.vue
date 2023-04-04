b39e2d537fa2c509e0257851649b88c680d42e0d
Abyan Falah <abyanf410@gmail.com>
2023-04-05 06:44:56 +0700

    Update TodoApp.vue: UI building

--- src/apps/TodoApp.vue
+++ src/apps/TodoApp.vue
@@ -1,8 +1,106 @@
 <script setup>
+import { computed } from '@vue/reactivity';
+import { onMounted, ref, watch } from 'vue';
+import TaskList from '@/components/todoapp/TaskList.vue';
+
+const taskGroupList = ref([]);
+const isViewingTaskGroup = ref(false);
+const selectedTaskGroup = ref();
+
+
+function newTaskGroup() {
+  const taskGroupListLength = taskGroupList.value.length;
+  const newTaskGroup = {
+    id: taskGroupListLength,
+    color: 'white',
+    name: `Task group ${taskGroupListLength + 1}`
+  };
+
+  isViewingTaskGroup.value = false;
+  taskGroupList.value.push(newTaskGroup);
+  selectedTaskGroup.value = newTaskGroup;
+
+  setTimeout(() => {
+    isViewingTaskGroup.value = true;
+  });
+}
+
+function toggleSelectedTaskgroup(taskGroup) {
+  if (taskGroup == selectedTaskGroup.value) {
+    selectedTaskGroup.value = null;
+    isViewingTaskGroup.value = false;
+    return;
+  }
+
+  isViewingTaskGroup.value = false;
+  selectedTaskGroup.value = taskGroup;
+  setTimeout(() => {
+    isViewingTaskGroup.value = true;
+  }, 200);
+}
+
+const states = computed(() => {
+  return {
+    taskGroupList: taskGroupList.value,
+    isViewingTaskGroup: isViewingTaskGroup.value,
+    selectedTaskGroup: selectedTaskGroup.value,
+  };
+});
+
+watch(states, (newState) => {
+  newState = JSON.stringify(newState);
+  localStorage.setItem('todoapp', newState);
+});
+
+onMounted(() => {
+
+});
 </script>
 
 <template>
-  <div >
-    <h1>Todo app</h1>
+  <div :class="`h-100 bg-teal-lighten-4 pa-10`">
+    <v-app-bar title="Todolist app"
+               :class="`bg-teal`"
+               v-if="false"
+               flat></v-app-bar>
+
+    <v-row>
+
+      <!-- taskgroup list -->
+      <v-col cols="3">
+        <v-row>
+          <v-col cols="12"
+                 v-for="(taskGroup, index) in taskGroupList">
+            <v-btn class="w-100 text-capitalize text-truncate"
+                   :class="selectedTaskGroup.id == taskGroup.id ? `bg-${taskGroup.color}-lighten` : null"
+                   prepend-icon="mdi-format-list-bulleted"
+                   @click="toggleSelectedTaskgroup(taskGroup)">
+              {{ taskGroup.name }}
+            </v-btn>
+          </v-col>
+
+          <v-col>
+            <v-btn prepend-icon="mdi-plus"
+                   class="w-100 bg-teal"
+                   size="large"
+                   @click="newTaskGroup">
+              new task group
+            </v-btn>
+          </v-col>
+
+        </v-row>
+      </v-col>
+
+      <!-- taskgroup view -->
+      <v-col cols="6">
+        <v-scroll-x-transition>
+          <TaskList v-if="isViewingTaskGroup"
+                    :taskGroup="selectedTaskGroup" />
+        </v-scroll-x-transition>
+      </v-col>
+    </v-row>
+
+    <!-- selected task detail -->
+
   </div>
 </template>
