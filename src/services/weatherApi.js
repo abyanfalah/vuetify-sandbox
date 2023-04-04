66f0547522528d592bd0f09943928a87ec464738
Abyan Falah <abyanf410@gmail.com>
2023-04-04 20:29:40 +0700

    Update weatherApi.js: add weather list by wmo weathercode

--- src/services/weatherApi.js
+++ src/services/weatherApi.js
@@ -28,4 +28,38 @@ export default {
 
     return weatherData;
   },
+  getWeatherName: (weatherCode) => {
+    const weather = {
+      0: "Clear sky",
+      1: "Mainly clear",
+      2: "partly cloudy",
+      3: "and overcast",
+      45: "Fog",
+      48: "depositing rime fog",
+      51: "Light Drizzle",
+      53: "Moderate Drizzle",
+      55: "Dense Drizzle",
+      56: "Light Freezing Drizzle",
+      57: "Dense Freezing Drizzle",
+      61: "Slight Rain",
+      63: "Moderate Rain",
+      65: "Heavy Rain",
+      66: "Light Freezing Rain",
+      67: "Heavy Freezing Rain",
+      71: "Slight Snow fall",
+      73: "Moderate Snow fall",
+      75: "Heavy Snow fall",
+      77: "Snow grains",
+      80: "Slight Rain showers",
+      81: "Moderate Rain showers",
+      82: "Violent Rain showers",
+      85: "Slight Snow showers",
+      86: "Heavy Snow showers",
+      95: "Slight/Moderate Thunderstorm ",
+      96: "Thunderstorm with slight hail",
+      99: "Thunderstorm with heavy hail",
+    };
+
+    return capitalize.words(weather[weatherCode]);
+  },
 };
