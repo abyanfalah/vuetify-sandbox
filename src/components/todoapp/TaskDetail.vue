6d6b8621674ead9e1e77129730a162f4c963ba34
Abyan Falah <abyanf410@gmail.com>
2023-04-06 01:25:53 +0700

    Update TaskDetail.vue

--- src/components/todoapp/TaskDetail.vue
+++ src/components/todoapp/TaskDetail.vue
@@ -4,10 +4,6 @@ import { onBeforeMount, ref, watch } from 'vue';
 const props = defineProps(['selectedTask']);
 const task = ref({});
 
-watch(() => task.value, () => {
-  console.log('switching task');
-});
-
 onBeforeMount(() => {
   task.value = props.selectedTask;
 });
