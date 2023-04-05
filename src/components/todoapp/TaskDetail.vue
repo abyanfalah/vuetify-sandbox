888fd4ceab64b17329ae9dcd82bf077aad67fbb1
Abyan Falah <abyanf410@gmail.com>
2023-04-05 17:27:15 +0700

    Update TaskDetail.vue: task detail form

--- src/components/todoapp/TaskDetail.vue
+++ src/components/todoapp/TaskDetail.vue
@@ -14,7 +14,37 @@ onBeforeMount(() => {
 </script>
 
 <template>
-  <v-card class="pa-8">
-    {{ task }}
+  <v-card class="pa-8"
+          elevation="3"
+          v-if="task">
+    <v-card-title class="pa-0">
+      <v-text-field variant="underlined"
+                    color="teal"
+                    v-model="task.task"
+                    label="Task name"></v-text-field>
+    </v-card-title>
+
+    <v-combobox label="Priority"
+                variant="underlined"
+                v-model="task.priority"
+                density="comfortable"
+                :items="['Very High', 'High', 'Normal', 'Low', 'Optional']"></v-combobox>
+
+
+    <v-textarea label="Additional notes"
+                color="teal"
+                variant="outlined"></v-textarea>
+
+    <v-card-actions>
+      <v-btn icon="mdi-check"
+             variant="default"
+             color="teal"></v-btn>
+
+      <v-btn icon="mdi-delete"
+             variant="default"
+             color="red"></v-btn>
+
+    </v-card-actions>
+    <!-- {{ task }} -->
   </v-card>
 </template>
