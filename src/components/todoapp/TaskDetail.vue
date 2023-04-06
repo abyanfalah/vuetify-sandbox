e3379fe687ece189ce6e07b77591da24ba1ac10e
Abyan Falah <abyanf410@gmail.com>
2023-04-07 00:28:10 +0700

    Update TaskDetail.vue and TaskList.vue: mark task as done and delete task from task detail component

--- src/components/todoapp/TaskDetail.vue
+++ src/components/todoapp/TaskDetail.vue
@@ -1,48 +1,99 @@
 <script setup>
-import { onBeforeMount, ref, watch } from 'vue';
+import { onBeforeMount, onMounted, ref, watch } from 'vue';
 
+
+const emit = defineEmits(['markTaskDone', 'deleteTask']);
 const props = defineProps(['selectedTask']);
 const task = ref({});
 
-onBeforeMount(() => {
+const deleteTaskConfirmation = ref(false);
+
+onMounted(() => {
   task.value = props.selectedTask;
 });
 </script>
 
 <template>
-  <v-card class="pa-8"
+  <v-card class=""
           elevation="3"
           v-if="task">
-    <v-card-title class="pa-0">
-      <v-text-field variant="underlined"
-                    color="teal"
-                    v-model="task.task"
-                    label="Task name"></v-text-field>
-    </v-card-title>
+    <v-container class="pa-5 pb-0">
+      <v-card-title class="pa-0">
+        <v-text-field variant="underlined"
+                      color="teal"
+                      v-model="task.task"
+                      label="Task name"></v-text-field>
+      </v-card-title>
 
-    <!-- task priority -->
-    <v-combobox label="Priority"
-                variant="underlined"
-                v-model="task.priority"
-                density="comfortable"
-                :items="['Urgent', 'High', 'Normal', 'Low']"></v-combobox>
+      <!-- task priority -->
+      <v-combobox label="Priority"
+                  variant="underlined"
+                  v-model="task.priority"
+                  density="comfortable"
+                  :items="['Urgent', 'High', 'Normal', 'Low']"></v-combobox>
 
-    <!-- task additional nots -->
-    <v-textarea label="Additional notes"
-                color="teal"
-                v-model="task.notes"
-                variant="outlined"></v-textarea>
+      <!-- task additional nots -->
+      <v-textarea label="Additional notes"
+                  color="teal"
+                  v-model="task.notes"
+                  variant="outlined"></v-textarea>
+
+
+    </v-container>
+
+    <v-divider></v-divider>
+
 
     <!-- buttons -->
-    <v-card-actions>
-      <v-btn icon="mdi-check"
+    <v-card-actions class="py-3 ">
+      <v-btn v-if="!task.isDone"
+             prepend-icon="mdi-check"
              variant="default"
-             color="teal"></v-btn>
+             :disabled="deleteTaskConfirmation"
+             @click="emit('markTaskDone', task)"
+             color="success">
+        mark as done
+      </v-btn>
 
-      <v-btn icon="mdi-delete"
+      <v-sheet v-if="task.isDone"
+               class="text-disabled">
+        <v-icon icon="mdi-check"></v-icon>
+        Task is done
+      </v-sheet>
+
+      <v-btn prepend-icon="mdi-delete"
+             class="ms-auto"
+             @click="deleteTaskConfirmation = true"
              variant="default"
-             color="red"></v-btn>
+             color="red">
+        <span v-if="deleteTaskConfirmation">
+          Are you sure?
+        </span>
+        <span v-else>
+          delete this task
+        </span>
+      </v-btn>
 
     </v-card-actions>
+
+    <v-expand-transition>
+      <v-card v-show="deleteTaskConfirmation">
+
+        <v-card-actions class="d-flex  justify-end">
+          <v-btn variant="outlined"
+                 color="red"
+                 @click="emit('deleteTask', task)">
+            Yes
+          </v-btn>
+
+          <v-btn variant="tonal"
+                 color="primary"
+                 @click="deleteTaskConfirmation = false">
+            No
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-expand-transition>
+
   </v-card>
 </template>
--- src/components/todoapp/TaskList.vue
+++ src/components/todoapp/TaskList.vue
@@ -2,9 +2,9 @@
 import { onBeforeMount, ref, watch } from 'vue';
 import todoAppModules from '@/services/todoAppModules';
 
-const emit = defineEmits(['seeTaskDetail', 'deleteTaskGroup', 'closeTaskGroup']);
+const emit = defineEmits(['seeTaskDetail', 'markTaskDone', 'deleteTaskGroup', 'closeTaskGroup', 'clearCompletedTasks']);
 const props = defineProps(['taskGroup']);
-// const getColorByPriority = todoAppModules.getColorByPriority;
+const getColorByPriority = todoAppModules.getColorByPriority;
 
 const taskGroup = ref({});
 const selectedTask = ref(null);
@@ -45,6 +45,7 @@ function toggleSelectTask(task) {
 
 }
 
+
 onBeforeMount(() => {
   taskGroup.value = props.taskGroup;
 });
@@ -53,71 +54,103 @@ onBeforeMount(() => {
 <template>
   <v-card :class="`bg-${taskGroup.color}`"
           elevation="3"
-          class="rounded  pa-8">
+          class="rounded">
     <v-card-title class="pa-0">
-      <v-text-field v-model="taskGroup.name"
-                    label="Group name"
-                    density="comfortable"
-                    variant="underlined"
-                    prepend-icon="mdi-close-circle"
-                    @click:prepend="emit('closeTaskGroup', taskGroup)"
-                    color="teal"
-                    :autofocus="taskGroup.name.toLowerCase().indexOf('task group ') > -1 ? true : false"
-                    append-inner-icon="mdi-palette"
-                    @click:append-inner="changeColor"
-                    hide-details>
-      </v-text-field>
+      <v-toolbar class="py-3"
+                 color="white">
+        <template v-slot:prepend>
+          <v-btn icon="mdi-dots-vertical"
+                 id="menu-activator"></v-btn>
 
+          <v-menu activator="#menu-activator"
+                  transition="scroll-x-transition">
+            <v-list class="px-3">
+              <v-list-item prepend-icon="mdi-palette"
+                           title="Change color"
+                           class="rounded"
+                           @click="changeColor"></v-list-item>
+
+              <v-list-item prepend-icon="mdi-broom"
+                           title="Clear completed tasks"
+                           class="rounded"
+                           @click="emit('clearCompletedTasks')"></v-list-item>
+
+              <v-list-item prepend-icon="mdi-delete"
+                           title="Delete this task group"
+                           class="bg-red rounded"
+                           variant="outlined"
+                           @click="emit('deleteTaskGroup', taskGroup)"></v-list-item>
+            </v-list>
+          </v-menu>
+        </template>
+
+
+        <v-text-field class="mx-3"
+                      v-model="taskGroup.name"
+                      label="Group name"
+                      density="comfortable"
+                      variant="underlined"
+                      color="teal"
+                      :autofocus="taskGroup.name.toLowerCase().indexOf('task group ') > -1 ? true : false"
+                      hide-details>
+        </v-text-field>
+
+        <template v-slot:append>
+          <!-- <v-btn icon="mdi-palette"
+                                                                                                                                     @click="changeColor"></v-btn> -->
+
+          <v-btn icon="mdi-close-circle"
+                 @click="emit('closeTaskGroup', taskGroup)"></v-btn>
+        </template>
+      </v-toolbar>
     </v-card-title>
 
-    <!-- task item -->
-    <v-sheet class="mb-5 mt-10">
-      <v-sheet v-for="(task, index) in taskGroup.taskList"
-               :class="task.isDone ? 'text-disabled' : ''"
-               class="mb-3 py-1 px-2 border d-flex justify-space-between align-center rounded">
+    <!-- task items -->
+    <v-card-item class="px-5">
+      <v-sheet class="my-5">
+        <v-sheet v-for="(task, index) in taskGroup.taskList"
+                 :class="task.isDone ? 'text-disabled' : ''"
+                 class="mb-3 py-1 px-2 border d-flex justify-space-between align-center rounded">
 
-        <!-- checkbox -->
-        <v-checkbox-btn v-model="task.isDone"
-                        @click="task.toggleDoneState"></v-checkbox-btn>
+          <!-- checkbox -->
+          <v-checkbox-btn v-model="task.isDone"
+                          @click="emit('markTaskDone', task)"></v-checkbox-btn>
 
-        <!-- task text -->
-        <input type="text"
-               @keydown.enter="$event.target.blur()"
-               v-model="taskGroup.taskList[index].task"
-               class="w-100"
-               :class="task.isDone ? 'text-disabled text-decoration-line-through' : ''">
+          <!-- task text -->
+          <input type="text"
+                 @keydown.enter="$event.target.blur()"
+                 v-model="taskGroup.taskList[index].task"
+                 class="w-100"
+                 :class="task.isDone ? 'text-disabled text-decoration-line-through' : ''">
 
-        <!-- see detail btn -->
-        <v-btn :icon="`mdi-chevron-${selectedTask == task ? 'left' : 'right'}`"
-               flat
-               :class="selectedTask == task ? 'bg-black' : 'bg-transparent'"
-               @click="toggleSelectTask(task)"
-               density="comfortable"></v-btn>
+          <!-- see detail btn -->
+          <v-btn :icon="`mdi-chevron-${selectedTask == task ? 'left' : 'right'}`"
+                 flat
+                 :class="selectedTask == task ? 'bg-black' : 'bg-transparent'"
+                 @click="toggleSelectTask(task)"
+                 density="comfortable"></v-btn>
+        </v-sheet>
+
+        <!-- task input -->
+        <v-text-field hide-details
+                      density="comfortable"
+                      class="rounded mt-5"
+                      placeholder="Add new task"
+                      clearable
+                      :autofocus="taskGroup.name.toLowerCase().indexOf('task group ') < 0 ? true : false"
+                      label="+ New task"
+                      append-inner-icon="mdi-plus"
+                      @click:append-inner="addTask"
+                      @keydown.enter="addTask"
+                      v-model="taskInput"
+                      color="teal"
+                      variant="outlined"></v-text-field>
+
       </v-sheet>
+    </v-card-item>
 
-      <!-- task input -->
-      <v-text-field hide-details
-                    density="comfortable"
-                    class="rounded mt-5"
-                    placeholder="Add new task"
-                    clearable
-                    :autofocus="taskGroup.name.toLowerCase().indexOf('task group ') < 0 ? true : false"
-                    label="+ New task"
-                    append-inner-icon="mdi-plus"
-                    @click:append-inner="addTask"
-                    @keydown.enter="addTask"
-                    v-model="taskInput"
-                    color="teal"
-                    variant="outlined"></v-text-field>
+    <!-- buttons -->
 
-    </v-sheet>
-
-    <v-card-actions>
-      <v-btn color="red"
-             variant="tonal"
-             @click="emit('deleteTaskGroup', taskGroup)"
-             prepend-icon="mdi-delete">Delete task group</v-btn>
-    </v-card-actions>
   </v-card>
 </template>
 
