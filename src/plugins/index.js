74671cf60735ce6c0aaec3307f8c7a9eb0a11d45
Abyan Falah <abyanf410@gmail.com>
2023-04-07 19:19:11 +0700

    add pinia

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -5,10 +5,13 @@
  */
 
 // Plugins
-import { loadFonts } from './webfontloader'
-import vuetify from './vuetify'
+import { loadFonts } from "./webfontloader";
+import vuetify from "./vuetify";
+import { createPinia } from "pinia";
+const pinia = createPinia();
 
-export function registerPlugins (app) {
-  loadFonts()
-  app.use(vuetify)
+export function registerPlugins(app) {
+  loadFonts();
+  app.use(vuetify);
+  app.use(pinia);
 }
