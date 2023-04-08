88992a747b587ccaf305990d186a74207aac50a4
Abyan Falah <abyanf410@gmail.com>
2023-04-08 15:06:35 +0700

    favicon and default document title

--- src/main.js
+++ src/main.js
@@ -16,5 +16,5 @@ import { registerPlugins } from "@/plugins";
 const app = createApp(App);
 
 registerPlugins(app);
-document.title = "Vuetify sandbox";
+document.title = "Abyanf's sandbox";
 app.mount("#app");
