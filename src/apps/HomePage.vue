7a9e92ebfce6d128e4a116053acfeebb5f28c904
Abyan Falah <abyanf410@gmail.com>
2023-04-11 16:05:01 +0700

    Update HomePage.vue: enable funny message

--- src/apps/HomePage.vue
+++ src/apps/HomePage.vue
@@ -9,7 +9,8 @@ function thatsIt() {
     <v-row class="h-screen">
       <v-col class="d-flex justify-center align-center flex-column pa-10">
         <h1 class="text-h2 text-center">Hi! Welcome to this sandbox of mine!</h1>
-        <v-btn class="mt-10">
+        <v-btn class="mt-10"
+               @click="thatsIt">
           Cool! Tell me more about it!
         </v-btn>
       </v-col>
