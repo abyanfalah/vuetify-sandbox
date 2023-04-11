4e77ee79aad13770d97741cd4eab689f1b1d74e7
Abyan Falah <abyanf410@gmail.com>
2023-04-11 16:05:59 +0700

    Update HomePage.vue

--- src/apps/HomePage.vue
+++ src/apps/HomePage.vue
@@ -1,6 +1,6 @@
 <script setup>
 function thatsIt() {
-  alert(`That's it. Ironically, this "home" page page is not ready yet. But that doesn't make visitors became "homeless" hehehe just kidding :)`);
+  alert(`That's it. Ironically, this "home" page page is not ready yet. But that doesn't make visitors  "homeless" hehehe just kidding :)`);
 }
 </script>
 
